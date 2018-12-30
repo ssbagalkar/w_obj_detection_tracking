5bca07255ccc668110ff24b4fb07b57236fce22b
Saurabh <sbagalka@asu.edu>
2018-12-30 14:06:29 -0600

    generate train test img folders

--- data_preprocessing/train_test_split_data.py
+++ data_preprocessing/train_test_split_data.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 import csv
+import shutil
 
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
@@ -15,19 +16,19 @@ def find_csv_filenames( path_to_dir, suffix=".csv" ):
                 csv_files.append(os.path.join(path_to_dir,case,filename))
     return csv_files
 
-    # return [ filename for filename in all_case_locations if filename.endswith( suffix ) ]
 
-
-def generate_train_test_csv(train_filenames, test_filenames,train_test_dir ):
+def generate_train_test_csv(train_filenames, test_filenames, train_test_dir ):
 
     # declare all paths
     original_csv_path = os.path.join(train_test_dir, 'walmart_labels.csv')
     print("Creating train folder...")
-    os.makedirs(os.path.join(train_test_dir,'train'))
+    os.makedirs(os.path.join(train_test_dir,'train', 'data'))
+    os.makedirs(os.path.join(train_test_dir, 'train', 'labels'))
     print("Creating test folder...")
-    os.makedirs(os.path.join(train_test_dir, 'test'))
-    train_csv_path = os.path.join(train_test_dir,'train', 'train_labels.csv')
-    test_csv_path = os.path.join(train_test_dir, 'test','test_labels.csv')
+    os.makedirs(os.path.join(train_test_dir, 'test', 'data'))
+    os.makedirs(os.path.join(train_test_dir, 'test', 'labels'))
+    train_csv_path = os.path.join(train_test_dir,'train','labels', 'train_labels.csv')
+    test_csv_path = os.path.join(train_test_dir, 'test', 'labels', 'test_labels.csv')
 
     with open(original_csv_path, mode='r') as csv_file_in, open(train_csv_path, mode='w',newline='') as csv_train, open(test_csv_path, mode='w', newline='') as csv_test:
         csv_reader = csv.DictReader(csv_file_in)
@@ -70,6 +71,30 @@ def generate_train_test_csv(train_filenames, test_filenames,train_test_dir ):
         print(f'Written {num_lines_test} lines in test.csv.')
         print("Train and Test csv's generated")
 
+def generate_train_test_data(train_filenames, test_filenames, train_test_dir, input_dir ):
+    print("\t\t")
+    print("Copying all images in training set...")
+    print("Training set has {} images...".format(len(train_filenames)))
+    for train_file in train_filenames:
+        train_img = train_file.split(".")[0]+".png"
+        case = train_img.split("_frame")[0]
+        print("Copying {} to train folder".format(train_img))
+        shutil.copy2(os.path.join(input_dir,case,'frames',train_img),os.path.join(train_test_dir,'train','data'))
+    print("All train images copied")
+    print("\t")
+
+    print("Copying all images in test set...")
+    for test_file in test_filenames:
+        test_img = test_file.split(".")[0]+".png"
+        case = test_img.split("_frame")[0]
+        print("Copying {} to test folder...".format(test_img))
+        shutil.copy2(os.path.join(input_dir,case,'frames',test_img),os.path.join(train_test_dir,'test','data'))
+    print("All test images copied.")
+
+
+
+
+
 def split_into_train_test(input_dir, output_dir):
     if os.path.isdir(output_dir):
         print("Train and Test folders already present. Skipping.......")
@@ -102,9 +127,8 @@ def split_into_train_test(input_dir, output_dir):
         print("Length of train set --> {}".format(len(train_filenames)))
         print("Length of test set --> {}".format(len(test_filenames)))
 
-        # # Start reading the csv files to copy each row of associated filename to train and test accordingly
         generate_train_test_csv(train_filenames, test_filenames, train_test_dir=output_dir)
-        #
-        # generate_train_test_img_folders(input_dir, output_dir, train_filenames, test_filenames, resized, resize_factor)
+        generate_train_test_data(train_filenames, test_filenames, train_test_dir=output_dir, input_dir=input_dir)
+
 
 
